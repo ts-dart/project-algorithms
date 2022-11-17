cdd02fcba91f64b81a219867286f5c914bf50e06
trybe-tech-ops <82593112+trybe-tech-ops@users.noreply.github.com>
2022-11-17 19:48:39 -0300

    Initial commit

---
+++ tests/encrypt/mocks.py
@@ -0,0 +1,69 @@
+def encrypt_does_nothing(message: str, key: int):
+    pass
+
+
+def encrypt_reversed_mod(message: str, key: int):
+    if not isinstance(key, int):
+        raise TypeError("tipo inválido para key")
+
+    if not isinstance(message, str):
+        raise TypeError("tipo inválido para message")
+
+    if key not in range(1, len(message)):
+        return "".join(reversed(message))
+
+    part_one = reversed(message[:key])
+    part_two = reversed(message[key:])
+
+    if key % 2:
+        part_two, part_one = part_one, part_two
+
+    return "".join(part_one) + "_" + "".join(part_two)
+
+
+def encrypt_any_range_for_key(message: str, key: int):
+    if not isinstance(key, int):
+        raise TypeError("tipo inválido para key")
+
+    if not isinstance(message, str):
+        raise TypeError("tipo inválido para message")
+
+    part_one = reversed(message[:key])
+    part_two = reversed(message[key:])
+
+    if not key % 2:
+        part_two, part_one = part_one, part_two
+
+    return "".join(part_one) + "_" + "".join(part_two)
+
+
+def encrypt_sorted_not_reversed(message: str, key: int):
+    if not isinstance(key, int):
+        raise TypeError("tipo inválido para key")
+
+    if not isinstance(message, str):
+        raise TypeError("tipo inválido para message")
+
+    if key not in range(1, len(message)):
+        return "".join(reversed(message))
+
+    part_one = sorted(message[:key])
+    part_two = sorted(message[key:])
+
+    if not key % 2:
+        part_two, part_one = part_one, part_two
+
+    return "".join(part_one) + "_" + "".join(part_two)
+
+
+def encrypt_no_type_validation(message: str, key: int):
+    if key not in range(1, len(message)):
+        return "".join(reversed(message))
+
+    part_one = reversed(message[:key])
+    part_two = reversed(message[key:])
+
+    if not key % 2:
+        part_two, part_one = part_one, part_two
+
+    return "".join(part_one) + "_" + "".join(part_two)
