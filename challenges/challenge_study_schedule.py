17b532b9baa6e20d4dafbc8323111f98383fb3c1
thiago <thiagoedusan5.11@outlook.com>
2022-11-21 22:38:39 -0300

    feat: Foi desenvolvida a função study_schedule

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,17 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    arr, result = [], ''
+    for period in permanence_period:
+        if isinstance(period[0], str) or isinstance(period[1], str)\
+                or isinstance(target_time, tuple):
+            result = None
+
+        if not period[0] or not period[1] or not target_time:
+            result = None
+
+        if result is None:
+            return result
+
+        if period[0] <= target_time <= period[1]:
+            arr.append(True)
+
+    return len(arr)
