2dfec00f09c0b5ad136a261fa6fbf8dd34296355
thiago <thiagoedusan5.11@outlook.com>
2022-11-23 17:29:26 -0300

    chore: fazendo alterações para linter

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,17 +1,22 @@
 def study_schedule(permanence_period, target_time):
-    arr, result = [], ''
+    arr = []
     for period in permanence_period:
-        if isinstance(period[0], str) or isinstance(period[1], str)\
-                or isinstance(target_time, tuple):
-            result = None
-
-        if not period[0] or not period[1] or not target_time:
-            result = None
-
-        if result is None:
-            return result
+        if filters(period, target_time) is None:
+            return None
 
         if period[0] <= target_time <= period[1]:
             arr.append(True)
 
     return len(arr)
+
+
+def filters(period, target_time):
+    result = ''
+    if isinstance(period[0], str) or isinstance(period[1], str)\
+            or isinstance(target_time, tuple):
+        result = None
+
+    if not period[0] or not period[1] or not target_time:
+        result = None
+
+    return result
